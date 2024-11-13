126a87762b7875823db48e270619e2385370db08
limematheuz <mat.rib.lima@gmail.com>
2024-11-13 23:59:36 +0100

    complete M7E2

--- src/M7/M7E2.java
+++ src/M7/M7E2.java
@@ -6,25 +6,30 @@ import java.util.ArrayList;
 public class M7E2 {
     public static void main(String[] args) {
 
-        float numNotas = Float.parseFloat(JOptionPane.showInputDialog(null, "Introduce un numero de notas a introducir", "Introducir notas", JOptionPane.QUESTION_MESSAGE));
-
-        float notasAlumnos = Float.parseFloat(JOptionPane.showInputDialog(null, "Introduce la nota del alumno", "Introducir notas", JOptionPane.QUESTION_MESSAGE));
+        float numNotas = Float.parseFloat(JOptionPane.showInputDialog(null, "Cuantas notas quieres introducir?", " Introducir numero de notas ", JOptionPane.QUESTION_MESSAGE));
 
         ArrayList<Float> notas = new ArrayList<>();
 
         float resultado = 0;
 
-        for (float i = 1; i <= numNotas; i++) {
-            JOptionPane.showInputDialog(null, "Introduce la nota del alumno", "Introducir notas", JOptionPane.QUESTION_MESSAGE);
-            notas.add(notasAlumnos);
-            resultado += notasAlumnos;
+        for (int i = 1; i <= numNotas; i++) {
+            float notaAlumnos = Float.parseFloat(JOptionPane.showInputDialog(null, "Introduce la nota del alumno", "Introducir notas", JOptionPane.QUESTION_MESSAGE));
+            notas.add(notaAlumnos);
+            resultado += notaAlumnos;
         }
 
-        JOptionPane.showMessageDialog(null, "Las notas introducidas son: " + notas, "Notas Introducidas", JOptionPane.INFORMATION_MESSAGE);
-        for (float nota : notas) {
-            JOptionPane.showMessageDialog(null, "Nota: " + nota, "Nota", JOptionPane.INFORMATION_MESSAGE);
-        }
+        JOptionPane.showMessageDialog(null, "Las notas introducidas son: \n" + notas, "Notas Introducidas", JOptionPane.INFORMATION_MESSAGE);
 
-        JOptionPane.showMessageDialog(null, "La media de las notas introducidas es: " + resultado / numNotas, "Media de las notas", JOptionPane.INFORMATION_MESSAGE);
+        float resultadoFinal = resultado / numNotas;
+
+        JOptionPane.showMessageDialog(null, "La media de las notas introducidas es: " + resultadoFinal, "Media de las notas", JOptionPane.INFORMATION_MESSAGE);
+
+        if (resultadoFinal < 5) {
+            JOptionPane.showMessageDialog(null, "La nota media de la clase está suspendida. Los alumnos/as deberían preguntar sus dudas y trabajar más", "Resultado Final: ", JOptionPane.WARNING_MESSAGE);
+        } else if (resultadoFinal < 7) {
+            JOptionPane.showMessageDialog(null, "La nota media de la clase es buena, pero los alumnos/as deberían mejorar el trabajo personal", "Resultado Final: ", JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(null, "¡Enhorabuena! La nota media de la clase se corresponde con el esfuerzo realizado", "Resultado Final: ", JOptionPane.INFORMATION_MESSAGE);
+        }
     }
 }
---
+++ src/M7/M7E6.java
@@ -0,0 +1,4 @@
+package M7;
+
+public class M7E6 {
+}
