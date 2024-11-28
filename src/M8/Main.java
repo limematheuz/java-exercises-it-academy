ba013c7101f5d5c6a713670e520b3a8ecd71b01d
limematheuz <mat.rib.lima@gmail.com>
2024-11-29 00:09:08 +0100

    modify M7.3E1

--- src/M8/Main.java
+++ src/M8/Main.java
@@ -5,7 +5,9 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         Ordenador ordenador1 = new Ordenador("HP", "Pavillon");
+
         Ordenador ordenador2 = new Ordenador("HP", "Pavillon", "Intel i7", 16, 512);
+
         Ordenador ordenador3 = new Ordenador("Lenovo", "YOGA", "Ryzen 7", 16, 512);
 
 //        JOptionPane.showMessageDialog(null, ordenador1);
@@ -16,6 +18,8 @@ public class Main {
         ordenador1.setMemoriaRAM(4);
         ordenador1.setCapacidadDiscoDuro(256);
         JOptionPane.showMessageDialog(null, ordenador1);
+
+//        JOptionPane.showMessageDialog(null, ordenador1.getMarca());
 
         JOptionPane.showMessageDialog(null, ordenador2.ejecutarPrograma("Intellij"));
     }
